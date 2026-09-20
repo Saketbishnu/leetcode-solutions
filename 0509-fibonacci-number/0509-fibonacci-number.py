9b2ab8bac3fbe0d2216778b144d2af129c76b89b
Saket Bishnu <127406716+Saketbishnu@users.noreply.github.com>
2026-09-20 18:02:11 +0530

    Time: 39 ms (91.25%), Space: 19.2 MB (58.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
+        if n==0 or n==1:
             return n
-        #recursive case
-        return self.fib(n-1) + self.fib(n-2)
+        a=0
+        b=1
+        for i in range(2, n+1):
+            temp=a
+            a=b
+            b=temp+a
+        return b
 
 
-        
